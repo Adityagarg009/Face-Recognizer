b8db22b13c5768f314c867a395d2352369791d76
unknown <adipoketrainer@gmail.com>
2023-12-03 16:47:13 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,3 @@
+from PIL import Image
+import cv2
+recognizer= cv2.face.LBPHFaceRecognizer_Create()
---
+++ register.py
@@ -0,0 +1,26 @@
+import cv2 as cv
+import pandas as pd
+
+face_cascade = cv.CascadeClassifier("content\haarcascade_frontalface_default.xml")
+cam=cv.VideoCapture(0)
+Id = int(input("enter your id number"))
+Name = input("Enter your name")
+df2 = pd.Dataframe({"Id": [Id], "Name": [Name]})
+df = pd.concat([ df, df2]).drop_duplicates(),reset_index(drop=True)
+df.to_csv("content\oye.csv",index=False)
+sampleNum = 0
+while True:
+    ret, img = cam.read()
+    faces=face_cascade.detectMultiScale(img,1.3,5)
+    for(x, y ,w ,h) in faces:
+        cv.rectangle(img ,(x, y),(x+w, y+h), (144,1,1),3)
+        sampleNum = sampleNum+1
+        #saving data in oye
+        cv.imwrite("content" +Id+ '.' + (sampleNum + ".jpg"))
+        cv.imshow('frame' ,img)
+    if cv.waitKey(100) & 0xFF == ord('q'):
+        break
+    elif sampleNum>10:
+        break
+    cam.release()
+    cv.destroyAllWindows()
